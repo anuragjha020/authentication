1381a654cf1cca8672bd7157decc614d096f5aa6
Anurag Jha <anuragjha020@gmail.com>
2024-12-13 10:40:41 +0530

    style update commit

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,7 +1,21 @@
 import "../styles/dashboard.css";
 
 function Dashboard() {
-  return <div className="dashboard-container">Welcome to my word!</div>;
+  return (
+    <div className="dashboard-container">
+      {" "}
+      <div class="hero">
+        <h1>
+          Welcome to <span>Alohabid</span>
+        </h1>
+        <p>Transform the way you want with ease and efficiency.</p>
+        <div class="buttons">
+          <button>Get Started</button>
+          <button class="secondary">Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
